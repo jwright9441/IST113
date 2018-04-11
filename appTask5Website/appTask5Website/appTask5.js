4f8f9dd824460114d26620f2c5964c671906ecfe
jwright9441 <31801319+jwright9441@users.noreply.github.com>
2018-04-11 09:37:28 -0400

    razor app

---
+++ appTask5Website/appTask5Website/appTask5.js
@@ -0,0 +1,103 @@
+var myKey = "listValues";
+var myItems = [];
+
+function runApp() {
+
+  if(localStorage.getItem(myKey) !== null) {
+    let myItemsString = localStorage.getItem(myKey);
+
+    myItems = JSON.parse(myItemsString);
+
+    $(myItems).each(function() {
+      createItem(this);
+    });
+  }
+  else {
+    createItem("List Goes Here");
+  }
+
+  //button
+  $("#buttonElement").on("click", function() {
+
+    //create button
+    let taskValue = $("#taskInput").val();
+
+    //call create item function
+    createItem(taskValue);
+
+
+
+    //push item to array
+    myItems.push(taskValue);
+
+    //save array
+    saveItems();
+
+  });
+
+  //Form display button
+  $("#formButton").on("click", function() {
+    if ($(".my-form").css("visibility") == "hidden") {
+      $(".my-form").css("visibility", "visible");
+    }
+    else {
+      $(".my-form").css("visibility", "hidden");      
+    }
+  });
+
+}
+
+
+  //create items function
+  function createItem(itemValue) {
+    let $newElem = $("<li></li>").text(itemValue);
+    $("#taskList").append($newElem);
+
+    //delete button
+    let delButton = $('<button>',
+    {
+      text: "delete",
+      click: function() {
+        removeItem($newElem);
+        $(this.parentNode).remove();
+
+      }
+    });
+   $newElem.append(delButton);
+  }
+
+  function removeItem(item) {
+
+    let index = item.index();
+    myItems.splice(index, 1);
+    saveItems();
+
+  }
+
+  //save items function
+  function saveItems() {
+    let myItemsString = JSON.stringify(myItems);
+    localStorage.setItem(myKey, myItemsString);
+  }
+
+  $("#delAll").on("click", function() {
+    myItems = [];
+    $("ul").empty();
+    saveItems();
+  })
+
+//Razor Reload Function
+function razorReload(email, uName, posted) {
+    if (posted == 1) {
+        alert("Submitted\nEmail: " + email + "\nUser Name: " + uName);
+    }
+    else {
+        alert("welcome!");
+    }
+}
+
+
+//run the app
+$(function() {
+  runApp();
+});
